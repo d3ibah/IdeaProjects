8232f7e820da23a94095c466c5e261e1214f0e51
d3ibah <buae@ya.ru>
2017-06-13 02:43:56 +0300

    preapha

--- homeWork7.1/src/basepackage/Bankomat.java
+++ homeWork7.1/src/basepackage/Bankomat.java
@@ -3,13 +3,13 @@ package basepackage;
 import java.util.Scanner;
 
 public class Bankomat {
-    private int count20 = 0;       // budet 2 publ metoda - vziat dengi i dat dengi
+    private int count20 = 0;
     private int count50 = 0;
     private int count100 = 0;
     private final int NUM20 = 20;
     private final int NUM50 = 50;
     private final int NUM100 = 100;
-    private int inkosacija = NUM20 * count20 + NUM50 * count50 + NUM100 * count100;
+    private int inkosacija;
     private OnBankomatListenner ui;
 
     public void setUi(OnBankomatListenner ui) {
@@ -21,54 +21,30 @@ public class Bankomat {
         System.out.println("Укажите сумму, которую хотите снять. Учитывайте, что банкомат  купюры " +
                 "номиналом 20, 50 и 100.");
         Scanner scanner = new Scanner(System.in);
-        int temp = scanner.nextInt();
-        System.out.println(temp);
+        int temp = scanner.nextInt();                                               // вводим переменную, в которой будет храниться снимаемая сумма
 
         // прописываем логику снятия денег. начинаем с простых вариантов
         if (temp % 10 != 0 || temp == 30 || temp < 20 || temp > inkosacija) {     // указываем простые условия при которых выдача денег не возможна
-            ui.onGetMoney(1);
+            ui.onGetMoney(1);                                                     // передаём в onGetMoney значение "1", которое должно вызвать сообщение о не верно заданной сумме
         }
-        /*if (temp == inkosacija) {
-            count20 = 0;
-            count50 = 0;
-            count100 = 0;
-            ui.onGetMoney(1);
+        else {
+            while (temp <= inkosacija && temp / NUM100 > 0 && count100 > 0) {       // пишу циклы, где за один проход отнимается по 1 у счетчика купюр
+                count100 = count100 - 1;
+                temp = temp - NUM100;                                              // также на следующий проход уменьшается сумма которую необходимо снять с банкомата
+            }
+            while (temp < inkosacija && temp / NUM50 > 0 && count50 > 0) {
+                count50 = count50 - 1;
+                temp = temp - NUM50;
+            }
+            while (temp < inkosacija && temp / NUM20 > 0 && count20 > 0) {
+                count20 = count20 - 1;
+                temp = temp - NUM20;
+            }
+            inkosacija = NUM20 * count20 + NUM50 * count50 + NUM100 * count100;
+            ui.onGetMoney(0);
+            System.out.println("На счету осталось: " + inkosacija + ", наминалом 20 - " + count20 + "шт, наминалом 50 - " + count50 + "шт, наминалом 100 - " + count100 + " шт.");
         }
-        if (temp == NUM20) {
-            count20 = count20 - 1;
-            ui.onGetMoney(1);
-        }
-        if (temp == NUM50) {
-            count50 = count50 - 1;
-            ui.onGetMoney(1);
-        }
-        if (temp == NUM100) {
-            count100 = count100 - 1;
-            ui.onGetMoney(1);
-        }*/
-        while (temp <= inkosacija && temp / NUM100 > 0 && count100 > 0){
-            count100 = count100 - 1;
-            temp = temp - NUM100;
-        }
-        while (temp < inkosacija && temp / NUM50 > 0 && count50 > 0){
-            count50 = count50 - 1;
-            temp = temp - NUM50;
-        }
-        while (temp < inkosacija && temp / NUM20 > 0 && count20 > 0){
-            count20 = count20 - 1;
-            temp = temp - NUM20;
-        }
-        System.out.println(inkosacija + " NUM20 - " + count20 + " NUM50 - " + count50 + " NUM100 - " + count100);
 
-
-
-        // если нужно снять 70 руб, то нужно сделать
-        //count20 = count20 - 1;
-        //count50 = count50 - 1;
-        // рогресс - длительное ожидание
-        //return 0;
-
-        //ui.onGetMoney(value);
     }
 
     public boolean setMoney() {
@@ -80,10 +56,8 @@ public class Bankomat {
         count50 = count50 + scanner.nextInt();
         System.out.println("Введите количество купюр номиналом 100:\t");
         count100 = count100 + scanner.nextInt();
-
-        System.out.println("Сейчас в банкомате храниться:" + (NUM20 * count20 + NUM50 * count50 + NUM100 * count100));
-        // если нужно положить 100 руб, то нужно сделать
-        //count20 = count100 + 1;
+        inkosacija = NUM20 * count20 + NUM50 * count50 + NUM100 * count100;
+        System.out.println("Сейчас в банкомате храниться:" + inkosacija);
 
         ui.onSetMoney(true);
 
--- homeWork7.1/src/basepackage/UI.java
+++ homeWork7.1/src/basepackage/UI.java
@@ -4,33 +4,18 @@ public class UI implements OnBankomatListenner{
 
     public void startUI(){
 
-        // sniatie deneg
         Bankomat bankomat = new Bankomat();
         bankomat.setUi(this);
-
-        // polozhit deneg
         boolean isOK = bankomat.setMoney();
-
         bankomat.getMoney();
-
-
     }
 
-
     public void onGetMoney(int money){
-        /*switch (money){
-            case (0):
-                System.out.println("Udacha dengi polucheny!");
-                break;
-            case (1):
-                System.out.println("Операция не может быть выполнена. Не корректно указана сумма снятия");
-                break;
-        }*/
         if (money == 1 ){
             System.out.println("Операция не может быть выполнена. Не корректно указана сумма снятия");
         }
         else {
-            System.out.println("Udacha dengi polucheny!");
+            System.out.println("Операция прошла успешно!");
         }
     }
 
